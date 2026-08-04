a60e51d0b705d83fe589067366b9f1ef41c95f0a
Eng-Moaz-Tello <shangahay1995@gmail.com>
2026-08-04 15:18:29 +0300

    update directorates, services all files depends on them

--- js/services.js
+++ js/services.js
@@ -1,19 +1,15 @@
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navLinks');
-document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.getElementById('searchInput');
-  const cards = document.querySelectorAll('.ministry-card');
+document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.getElementById("searchInput");
+  const cards = document.querySelectorAll(".ministry-card");
 
-  searchInput.addEventListener('input', (e) => {
-    const value = e.target.value.toLowerCase().trim();
+  if (searchInput) {
+    searchInput.addEventListener("input", (e) => {
+      const value = e.target.value.toLowerCase().trim();
 
-    cards.forEach(card => {
-      const text = card.textContent.toLowerCase();
-      if (text.includes(value)) {
-        card.style.display = 'flex';
-      } else {
-        card.style.display = 'none';
-      }
+      cards.forEach((card) => {
+        const text = card.textContent.toLowerCase();
+        card.style.display = text.includes(value) ? "" : "none";
+      });
     });
-  });
+  }
 });
